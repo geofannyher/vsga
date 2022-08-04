4fc5c1f235d85486f184f53c45bea39017cf7ad5
Geofannyher <geofannyher@gmail.com>
2022-08-05 01:36:18 +0700

    tugas 5 and 6

---
+++ tugas5_M_Geofany_Hermawan.php
@@ -0,0 +1,35 @@
+<?php
+    //OPERATOR ARITMATIKA
+    $nilai1 = 30;
+    $nilai2 = 10;
+    // penjumlahan
+    $penjumlahan = $nilai1 + $nilai2;
+    // pengurangan
+    $pengurangan = $nilai1 - $nilai2;
+    //perkalian
+    $perkalian = $nilai1 * $nilai2;
+    //pembagian
+    $pembagian = $nilai1 / $nilai2;
+
+    //menampilkan nilai 1 dan 2
+    echo "nilai 1 : " .$nilai1;
+    echo "<br/>";
+    echo "nilai 2 : " .$nilai2;
+    echo "<hr/>";
+    //menampilkan hasil penjumlahan
+    echo "<br/> PENJUMLAHAN <br/>";
+    echo "Hasil penjumlahan adalah : " .$penjumlahan;
+    echo "<br/>";
+    //menampilkan hasil perkalian
+    echo "<br/> PENGURANGAN <br/>";
+    echo "Hasil pengurangan adalah : " .$pengurangan;
+    echo "<br/>";
+    //menampilkan hasil perkalian
+    echo "<br/> PERKALIAN <br/>";
+    echo "Hasil perkalian adalah : " .$perkalian;
+    echo "<br/>";
+    ////menampilkan hasil pembagian
+    echo "<br/> PEMBAGIAN <br/>";
+    echo "Hasil pembagian adalah : " .$pembagian;
+    echo "<br/>";
+?>
